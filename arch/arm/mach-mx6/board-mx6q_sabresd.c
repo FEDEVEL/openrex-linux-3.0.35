41c4c78a177fd632d7102ae4992a431be5729aa6
root <martin.murin@fedevel.com>
2016-02-08 04:39:30 -0800

    Updated SD port selection and UART mapping

--- arch/arm/mach-mx6/board-mx6q_sabresd.c
+++ arch/arm/mach-mx6/board-mx6q_sabresd.c
@@ -2083,8 +2083,8 @@ static void __init mx6_sabresd_board_init(void)
 	   Mfgtools want emmc is mmcblk0 and other sd card is mmcblk1.
 	*/
 	//!mm imx6q_add_sdhci_usdhc_imx(3, &mx6q_sabresd_sd4_data);
-	//imx6q_add_sdhci_usdhc_imx(1, &mx6q_sabresd_sd2_data);
-	imx6q_add_sdhci_usdhc_imx(2, &mx6q_sabresd_sd3_data);
+	imx6q_add_sdhci_usdhc_imx(1, &mx6q_sabresd_sd2_data);
+	//!MM imx6q_add_sdhci_usdhc_imx(2, &mx6q_sabresd_sd3_data);
 	imx_add_viv_gpu(&imx6_gpu_data, &imx6q_gpu_pdata);
 	imx6q_sabresd_init_usb();
 	/* SATA is not supported by MX6DL/Solo */
